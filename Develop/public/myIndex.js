65804edc9a7f3b6073fec3ec05716d13c5f5f96e
garrett krage <garrett.krage@gmail.com>
2020-01-27 09:56:35 -0700

    latest work.

--- Develop/public/myIndex.js
+++ Develop/public/myIndex.js
@@ -4,11 +4,13 @@ const express = require("express");
 const path = require("path");
 const fs = require('fs');
 const data = require('../db/db.json')
+// var http = require('http');
+// var server = http.createServer(app);
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,14 +20,17 @@ app.use(express.json());
 
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, './notes.html'))
+
 })
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'))
+    // res.json(data)
 })
 
 app.get('/api/notes', function (req, res) {
-    res.json(data)
+    // res.json(data);
+    
 })
 
 app.post('/api/notes', function (req, res) {
@@ -35,6 +40,12 @@ app.post('/api/notes', function (req, res) {
     })
 })
 
+
+// app.use(express.bodyParser());
+// app.post('/notes', function(req, res) {
+//   console.log(req.body);
+//   res.send(200);
+// });
 
 
 
